662735e1e1e70e444017748b4f8570274c5e7101
unknown <willyumburjer@gmail.com>
2019-10-06 16:42:40 +0800

    Project upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// $(document).ready(function() {
+//   // Transition effect for navbar 
+//   $(window).scroll(function() {
+//     // checks if window is scrolled more than 500px, adds/removes solid class
+//     if($(this).scrollTop() > 500) { 
+//         $('.navbar').addClass('solid');
+//     } else {
+//         $('.navbar').removeClass('solid');
+//     }
+//   });
+// });
+// var rellax = new Rellax('.rellax', {
+//   speed: -2,
+//   center: true,
+//   wrapper: null,
+//   round: true,
+//   vertical: true,
+//   horizontal: false
+// });
+
